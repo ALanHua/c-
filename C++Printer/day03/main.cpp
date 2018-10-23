e5b22a3ea01bf201424e152414bc26a2cc31aca5
ALanHua <yhp-2009@qq.com>
2018-10-23 23:21:59 +0800

    day03_02

--- C++Printer/day03/main.cpp
+++ C++Printer/day03/main.cpp
@@ -56,15 +56,78 @@ public:
     }
 };
 
+class Buffer {
+    string name;
+public:
+    Buffer(const string& filename):name(filename){
+        cout << filename << "分配缓冲区"<< endl;
+    }
+    
+    ~Buffer(){
+        cout << name << "释放缓冲区"<< endl;
+    }
+};
+
+/*
+ p()表示0初始化
+ */
 class File {
     string path_;
+    Buffer* p;
 public:
-    File(const string& path = "无名"):path_(path){
-        cout << "创建文件" << path_ << endl;
+    File(const string& path = "无名"):path_(path),p(){
+        cout << this  <<"创建文件" << path_ << endl;
+    }
+    ~File(){
+        cout << this << path_ << " 释放" << endl;
+    }
+    
+    void open(){
+        p = new Buffer(path_);
+    }
+    void close(){
+        delete p;
+        p = NULL;
     }
 };
 
 string CSD1212Stu::teacher = "andy";
+
+/**
+ 析构函数：对象释放时会自动调用的函数，函数名为类名，总是无惨
+ 总是无惨
+ 默认析构函数:如果一个类没有定义析构函数，编译器会自动产生一个
+ 什么也不做的析构函数
+ */
+
+/**
+ 匿名对象：直接定义一个对象但不给提供名字，这个对象会在这个x语句之后j
+ 立即释放，编译器还会最大限度的优化性能，提供一次性匿名对象
+------
+ 类名(参数) ---没有变量名，对象只能当场使用
+ */
+class R{
+    int n; // 分子
+    int d; // 分母
+public:
+    R(int n,int d = 1):n(n),d(d){
+        cout << "R(" << n << '/' << d << ")" << endl;
+    }
+    R add(const R& r2){
+        int nd = d * r2.d;
+        int nn = d * r2.n + n * r2.d;
+//        R result(nn,nd);
+//        return result;
+        return R(nn,nd); // 匿名对象
+    }
+    void show(){
+        cout << n << '/' << d << endl;
+    }
+    ~R(){
+        cout << "~" << n << '/' << d << endl;
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
     
@@ -82,13 +145,17 @@ int main(int argc, const char * argv[]) {
     c2.show();
     cout << &c1 << "," << &c2 << endl;
     
-    File* p1 = new File;
+    File aFile("a");
     File* p2 = new File("/etc/passwd");
-    File* p3 = new File[3];
-    
-    delete[] p3;p3 = NULL;
+    p2->open();
+    p2->close();
     delete p2;p2 = NULL;
-    delete p1;p1 = NULL;
-
+    
+    R a1(2,5), b1(3,4);
+    a1.add(b1).show();
+    cout << "-------------" << endl;
+    a1.add(R(3,8)).show();
+    cout << "-------------" << endl;
+    
     return 0;
 }
