0ec920bedd3574d53ae844e49543bee854786112
ALanHua <yhp-2009@qq.com>
2018-11-01 00:10:49 +0800

    day05_02

--- C++Printer/day05/main.cpp
+++ C++Printer/day05/main.cpp
@@ -236,6 +236,141 @@ public:
         cout << "DoubleMan " << endl;
     }
 };
+        
+// 多态
+// 虚函数表: 编译器把这个类的全部虚函数的地址都存在一张表中
+// 每个这类对象里会藏一个指针指向这个虚函数表(在构造函数里做的)
+// 对象长度增加4（mac os是8）个字节
+
+/*
+利用多态实现类型识别
+dynamic_cast<子类类型&>(是父类类型对象) ---
+一般用这个结果来初始化一个子类类型的对象的引用或者当场使用。成功
+则一切顺利，失败会抛出l异常,终止程序
+dynamic_cast<子类*>(是父类类型对象地址) ---成功
+则一切顺利，失败则结果为NULL，常常使用这一种
+dynamic_cast 要求有虚函数。
+*/
+class Vehicle{
+public:
+//  如果希望在调用函数时系统根据对象的真实类型
+//  调用响应的函数，需要把这个函数声明成虚函数
+//  子类中可以覆盖这个函数这个函数自动成为虚函数
+//  覆盖[override],函数名和参数表都相同,返回类型也要一致
+//  父类中返回父类指针,子类中可以返回子类指针
+    virtual void stop(){
+        cout << "交通工具停止" << endl;
+    }// 虚函数
+};
+        
+class Car:public Vehicle{
+public:
+    void stop(){
+        cout << "汽车停止" << endl;
+    }
+};
+        
+class Bike:public Vehicle{
+public:
+    void stop(){
+        cout << "自行车停止" << endl;
+    }
+};
+
+class Light{
+public:
+    void stopvehicle(Vehicle& v){
+        v.stop();
+    }
+};
+
+class Animal{
+public:
+    virtual void eat(){
+        cout << "动物吃食物" << endl;
+    }
+    virtual void shout(){
+        cout << "动物叫" << endl;
+    }
+private:
+    string name_;
+    char   gender_;// 补齐
+};
+
+class Cat:public Animal{
+    void eat(){
+        cout << "猫吃猫娘" << endl;
+    }
+    void shout(){
+        cout << "猫叫" << endl;
+    }
+};
+        
+class Dog:public Animal{
+    void eat(){
+        cout << "狗吃大便" << endl;
+    }
+    void shout(){
+        cout << "狗叫" << endl;
+    }
+};
+
+class Jiafei:public Cat {
+    void eat(){
+        cout << "加菲吃面" << endl;
+    }
+    void shout(){
+        cout << "加菲叫" << endl;
+    }
+};
+
+class Person3{
+public:
+    void play(Animal& a){
+        a.eat();
+        a.shout();
+    }
+};
+
+class Person4 {
+public:
+    virtual void show(){}
+};
+
+class Teacher4 : public Person4{
+    
+};
+class CppTeacher4 : public Teacher4{
+    
+};
+class Student4 : public Person4{
+    
+};
+
+class Computer{
+public:
+    void start(){}
+};
+
+class Notebook:public Computer {
+
+};
+        
+class Company{
+public:
+    void test(Person4* p){
+        Student4* p1 = dynamic_cast<Student4*>(p);
+        if(p1){
+            cout << "是学生" << endl;
+        }
+
+        Teacher4* p2 = dynamic_cast<Teacher4*>(p);
+        if(p2){
+            cout << "是老师" << endl;
+        }
+        // ... 略
+    }
+};
 //
 /**
  const对象只能调用const成员函数
@@ -309,6 +444,58 @@ int main(int argc, const char * argv[]) {
     Person* p2 = new Teacher("andy","swift");
     p2->show();
     delete p2;
-    cout << "-----end-----" << endl;
+    cout << "--11---end-----" << endl;
+    
+    Light lght;
+    Car   bmw;
+    Bike   yj;
+    
+    lght.stopvehicle(bmw);
+    lght.stopvehicle(yj);
+    cout << "--22---end-----" << endl;
+    cout << sizeof(Animal) << endl;
+    Person3 pp;
+    Dog     pd;
+    Jiafei  pj;
+    pp.play(pd);
+    pp.play(pj);
+    /*
+     虚函数表实现过程
+     Animal      --- | &eat()
+     虚函数表     --- | &shout()
+    
+     Cat         --- | &Cat::eat()
+     虚函数表     --- | &Cat::shout()
+     Dog         --- | &Dog::eat()
+     虚函数表     --- | &Dog::shout()
+  
+     Cat         --- | &Cat::eat()
+     虚函数表     --- | &Cat::shout()
+     
+     play(a)
+     通过a.vptr的虚表指针去调用响应的函数
+     a.eat()   === a.vptr[0]()
+     a.shout() === a.vptr[1]()
+     play(d)
+     d.eat()   === d.vptr[0]()
+     d.shout() === d.vptr[1]()
+     */
+    cout << "--33---end-----" << endl;
+     /*无虚函数错误*/
+//    Computer* comp = new Notebook();
+//    dynamic_cast<Notebook*>(comp);
+    Person4* ps = new Student4();
+    Person4* pt = new Teacher4();
+    Person4* pc = new CppTeacher4();
+    
+    Company apple;
+    apple.test(ps);
+    apple.test(pt);
+    apple.test(pc);
+    
+    delete ps;
+    delete pt;
+    delete pc;
+    
     return 0;
 }
