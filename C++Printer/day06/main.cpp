55baaaee796148b53ee39f7bcfb1672a3e173466
ALanHua <yhp-2009@qq.com>
2018-11-03 00:03:25 +0800

    day06_03

--- C++Printer/day06/main.cpp
+++ C++Printer/day06/main.cpp
@@ -17,8 +17,22 @@ using namespace std;
  作为父类，由子类来实现覆盖那些纯虚函数。从而
  可以创建子类类型的对象，子类对象可以当场父类
  对象来引用，可以用父类指针指向子类对象
+ 
+ 使用多态d时必须通过父类指针或者引用来访问子类对象
+ 而不能重建一个父类对象
+ 
+ 通过父类指针动态是否子类对象时，默认调用父类的析构
+ 如果希望也能使用多态调用对象自己的析构，应该在父类中
+ 把析构函数也声明为虚函数。
+ 构造函数不能是虚函数更不能是存续函数
+ 析构函数不能是纯虚函数
+ 
+ string 类 c++ 风格的字符串类
+ 构造:string(const string& s);
+     string(int n,char c);
+ 运算符:<<,>>,=,+=,+,[]
  */
-class Graph {
+class Graph {// 抽象类
 public:
     Graph(int x,int y):x_(x),y_(y){}
     virtual double area() = 0; // 存续函数
@@ -72,6 +86,81 @@ public:
     }
 };
 
+class Coder {
+public:
+    virtual void code(const char* input,char* output) = 0;
+    virtual void decode(const char* input,char* output) = 0;
+};
+
+class rm:public Coder {
+public:
+    void code(const char* input,char* output){
+        cout << "用rm格式编码压缩" << endl;
+    }
+    
+    virtual void decode(const char* input,char* output){
+        cout << "用rm格式解压缩" << endl;
+    }
+};
+
+class divx:public Coder {
+public:
+    void code(const char* input,char* output){
+        cout << "用div格式编码压缩" << endl;
+    }
+    
+    virtual void decode(const char* input,char* output){
+        cout << "用dir格式解压缩" << endl;
+    }
+};
+
+class File {
+public:
+    void setcoder(Coder& c){
+        pc_ = &c;
+    }
+    void zip(){
+        const char* soure = "";
+        char* result = NULL;
+        pc_->code(soure, result);
+        cout << "保存到文件中" << endl;
+    }
+    void unzip(){
+        const char* soure = "";
+        char* result = NULL;
+        pc_->decode(soure, result);
+        cout << "播放到大片" << endl;
+    }
+private:
+    Coder* pc_;
+};
+
+
+class A {
+public:
+    A(){
+        cout << "A()" << endl;
+    };
+    virtual ~A(){
+        cout << "~A()" << endl;
+    }
+    virtual void dosomething(){
+        
+    }
+};
+
+class B :public A{
+public:
+    B(){
+        cout << "B()" << endl;
+    };
+    ~B(){
+        cout << "~B()" << endl;
+    }
+    virtual void dosomething(){
+    }
+};
+
 // 纯虚函数:没有必要或者不应该有函数体的虚函数，用 “=0”
 int main(int argc, const char * argv[]) {
     Circle c(8,8,10);
@@ -80,8 +169,26 @@ int main(int argc, const char * argv[]) {
     Computer::usergraph(c);
     Computer::usergraph(r);
     
+    rm r1;
+    divx d1;
+    File f;
     
+    f.setcoder(d1);
+    f.zip();
+    f.unzip();
     
+    f.setcoder(r1);
+    f.zip();
+    f.unzip();
+    
+    A* x[2];
+//    new和delete 需要一一对应
+    x[0] = new B();
+    x[1] = new A();
+    
+    //...
+    delete x[0];
+    delete x[1];
     
     return 0;
 }
