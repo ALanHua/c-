d8b688311cfbc9d7693495a650bca34b695ad53e
ALanHua <yhp-2009@qq.com>
2018-11-10 23:58:28 +0800

    day09_02_)1

--- C++Printer/day09/main.cpp
+++ C++Printer/day09/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <deque>
 
 // 测试简单，正式z程序不建议在这里使用全局
 using namespace std;
@@ -24,6 +25,20 @@ using namespace std;
     还支持+=一个整数称为随机迭代器。
     区间：两个位置之间的数据。含头不含尾。
  3,标准容器
+ 标准容器的共性:
+    构造：无惨构造，拷贝构造,区间构造(把一个区间内的数据都放到容器中)
+    析构：....
+    运算符：=,比较(<,<=,>=,==,!=)
+    交换： swap,swap(c1,c2),c1.swap(c2)
+    插入：insert(pos,elem) 其中pos是一个迭代器
+    删除：erase(pos),erase(beg,end)删除指定位置或者指定区间
+    清空：clear,
+    辅助：size(),empty(),max_size()
+    迭代器：封装的指针，作为容器的内部类型。四种：
+    iterator,const_iterator,reverse_iterator,const_reverse_iterator
+    接口：begin()返回指向第一个元素的迭代器，end()返回指向最后一个的迭代器
+    这倆构成的区间正好是容器中的所有元素，rbegin()返回指向反向的第一个元素
+    rend()返回反向的最后一个元素
  序列式容器
     vector   向量
     deque    双端队列
@@ -86,6 +101,7 @@ public:
 
 
 
+
 int main(int argc, const char * argv[]) {
     
     cout << A<int,10>::v_<< endl;
@@ -95,6 +111,29 @@ int main(int argc, const char * argv[]) {
     A<char,0>().show();
     A<short*,5>().show();
     A<long,8>().show();
+    
+    cout << "-------迭代器------" << endl;
+    deque<const char*> dcc;
+    deque<const char*>::iterator it,ie;
+    dcc.insert(dcc.begin(), "hello");
+    dcc.insert(dcc.begin(), "c++");
+    dcc.insert(dcc.end(), "welcome");
+    dcc.insert(--dcc.end(), "world");
+    
+    it = dcc.begin();
+    ie = dcc.end();
+    while (it != ie) {
+        cout << *it++ << " ";
+    }
+    cout << endl;
+    cout << "size " << dcc.size() << endl;
+    cout << "max_size " << dcc.max_size() << endl;
+    
+    while (!dcc.empty()) {
+        cout << *dcc.begin() << endl;
+        dcc.erase(dcc.begin());
+    }
+    cout << "size " << dcc.size() << endl;
     
     return 0;
 }
