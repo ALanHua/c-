7735616522a55468e3af16aff454be0750b0c558
ALanHua <yhp-2009@qq.com>
2020-08-02 23:06:20 +0800

    Reference

--- c++_step_by_step_2020/day02/main.cpp
+++ c++_step_by_step_2020/day02/main.cpp
@@ -7,14 +7,43 @@
 //
 
 #include <iostream>
+using namespace std;
 
 /*
- 引用(Refreance)
- */
+引用(Refreance) -- 变量的别名
+1，对引用做计算相当于对变量别名做计算
+2，定义是就必须初始化,不能改变指向
+3, 比指针更安全
+*/
+void testRef(void)
+{
+    int age = 10;
+    // refAge 相当于是age的别名
+    int& refAge = age;
+    int& ref1 = refAge;
+    int& ref2 = ref1;
+    
+    refAge = 20;
+    refAge += 50;
+    ref1 += 10;
+    ref2 += 10;
+    
+    cout << age << endl;
+}
+
+void swap2(int& v1,int& v2){
+    int tmp = v1;
+    v1 = v2;
+    v2 = tmp;
+}
 
 int main(int argc, const char * argv[]) {
     
+    int a = 10;
+    int b = 20;
     
+    swap2(a,b);
+    cout << a << " " << b << endl;
     
     return 0;
 }
