ba70098c056221b9e99a841e16664598709bb854
ALanHua <yhp-2009@qq.com>
2020-08-04 23:03:14 +0800

    compilation

--- c++_step_by_step_2020/day02/main.cpp
+++ c++_step_by_step_2020/day02/main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 1，对引用做计算相当于对变量别名做计算
 2，定义是就必须初始化,不能改变指向
 3, 比指针更安全
+4, 引用的本质就是指针，只是编译器弱化这个
 */
 void testRef(void)
 {
@@ -37,13 +38,38 @@ void swap2(int& v1,int& v2){
     v2 = tmp;
 }
 
+// 侧面/证明其本质
+struct student {
+    int &age;
+};
+
+/*
+ -- 通用寄存器
+ 64bit
+ RAX / RBX / RCX /RDX
+ 32bit
+ EAX / EBX /ECX / EDX
+ 16bit
+ AX / BX / CX / DX
+ 8bit
+ AH  --H high
+ AL  --L low
+ ---寄存器存储空间
+ 和指针一样,x64上8个字节
+ ---
+ R开头的64bit
+ E开头的32bit
+ 
+ */
+
 int main(int argc, const char * argv[]) {
     
     int a = 10;
     int b = 20;
-    
     swap2(a,b);
     cout << a << " " << b << endl;
     
+    cout << sizeof(student) << endl;
+
     return 0;
 }
