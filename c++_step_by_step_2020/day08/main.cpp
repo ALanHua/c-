a01322ca199ac64f72aa578c44153d225c431a6d
ALanHua <yhp-2009@qq.com>
2020-08-28 23:21:40 +0800

    面向对象F-2

--- c++_step_by_step_2020/day08/main.cpp
+++ c++_step_by_step_2020/day08/main.cpp
@@ -14,7 +14,13 @@ using namespace std;
  -------------------------
  对象型参数和返回值
  1,使用对象类型作为函数的参数或者返回值,可能产生一些不必要的中间对象
- 
+ -------------------------
+ 匿名对象(临时对象)
+ 1,没有变量名,没有被指针指向的对象，用完后马上调用析构
+ -------------------------
+ 隐式构造
+ 1,c++中存在隐式构造的现象:某些情况下,会隐式调用单参数的构造函数
+ 2,explicit关键字,静止隐式构造
  -------------------------
  */
 class Car {
@@ -28,6 +34,9 @@ public:
        cout << " Car(const Car& car) -" << this << endl;
     }
     
+    ~Car(){
+        cout << "~~Car() -" << this << endl;
+    }
     void run(){
         cout << "run()" << endl;
     }
@@ -42,18 +51,67 @@ Car test2(){
     return car;
 }
 
+Car test3(){
+    return Car();
+}
+
+class Person {
+    int m_age;
+public:
+    Person(){
+        cout << "Person() -" << this << endl;
+    }
+    
+   explicit  Person(int age):m_age(age){
+        cout << "Person(int) -" << this << endl;
+    }
+    
+    Person(const Person& person){
+        cout << "Person(const Person& person) -" << this << endl;
+    }
+    
+    ~Person(){
+        cout << "~~Person()-" << this << endl;
+    }
+    void display(){
+        cout << "display()- age is " << this->m_age << endl;
+    }
+};
 void testCar(void){
 //   平台不一样,结果不一样
     Car car;
+    car = test3();
     test(car);
     Car car2;
     car2 = test2();
     Car car3 = test2();
+//    匿名对象
+    Car().run();
+}
+void test4(Person person){
+    
+}
+// 隐式构造
+//Person test5(){
+//    return 40;
+//}
+
+void testPerson(void)
+{
+    Person p1;
+    Person p2(10);
+    Person p3 = p2;
+//  隐式构造
+//    Person p4 = 20;//  Person p4(20);
+//    test4(30);
+//    test5();
+//    p1 = 60;
+    
 }
 
 int main(int argc, const char * argv[]) {
     
-
+    testPerson();
     
     return 0;
 }
