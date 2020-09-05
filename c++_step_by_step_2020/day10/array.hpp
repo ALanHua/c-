68c9aef33a81f5a157e585683cee859cd783a49d
ALanHua <yhp-2009@qq.com>
2020-09-05 23:13:56 +0800

    其他面向对象语法B-4

--- c++_step_by_step_2020/day10/array.hpp
+++ c++_step_by_step_2020/day10/array.hpp
@@ -19,10 +19,14 @@ class Array {
     Item* m_data;   // 元素d首地址
     int m_size;    // 元素个数
     int m_capacity;// 容量
+    void checkIndex(int index);
+    int extendCapacity();
 public:
     Array(int capacity = 0);
     ~Array();
     void add(Item value);
+    void remove(int index);
+    void insert(int index,Item value);
     Item get(int index);
     int size();
     Item operator[](int index);
@@ -43,24 +47,74 @@ Array<Item>::~Array(){
 }
 
 template <typename Item>
+void Array<Item>::checkIndex(int index){
+    if (index < 0 || index >= m_size) {
+        throw "array index is overflow";
+    }
+}
+
+template <typename Item>
+int Array<Item>::extendCapacity(){
+    int extSize = m_size * 2;
+    Item* newData = new Item[extSize];
+    if (newData == NULL) {
+        return -1;
+    }
+    memcpy(newData, m_data, m_size * sizeof(int));
+    delete [] m_data;
+    m_data = newData;
+    m_capacity = extSize;
+    return 0;
+}
+
+template <typename Item>
 void Array<Item>::add(Item value){
     if (m_size == m_capacity) {
         // 扩容
-        m_capacity = m_size * 2;
-        Item* newData = new Item[m_capacity];
-        memcpy(newData, m_data, m_size * sizeof(int));
-        delete [] m_data;
-        m_data = newData;
+        if (!extendCapacity()) {
+            throw "no enough memory";
+        }
     }
     m_data[m_size++] = value;
 }
 
 template <typename Item>
-Item Array<Item>::get(int index){
-    if (index < 0 || index >= m_size) {
-        // 报错,抛异常
-        throw "array index is overflow";
+void Array<Item>::remove(int index){
+    checkIndex(index);
+    // 数据位置前移
+    for (int i = index; i < m_size - 1; i++) {
+        m_data[i] = m_data[i+1];
     }
+    m_size--;
+}
+
+template <typename Item>
+void Array<Item>::insert(int index,Item value){
+    if (index < 0) {
+        return;
+    }else if (index >= m_size){
+        // 插入在最后面
+        add(value);
+    }else{
+        if (m_size + 1 > m_capacity) {
+            // 扩容
+            if (!extendCapacity()) {
+                throw "no enough memory";
+            }
+        }
+        
+        for (int i = index; i < m_size; i++) {
+            m_data[i+1] = m_data[i];
+        }
+        m_data[index] = value;
+        m_size++;
+    }
+    
+}
+
+template <typename Item>
+Item Array<Item>::get(int index){
+    checkIndex(index);
     return m_data[index];
 }
 
--- c++_step_by_step_2020/day10/main.cpp
+++ c++_step_by_step_2020/day10/main.cpp
@@ -20,16 +20,32 @@ using namespace std;
 4,模板的声明和实现如果分离到.h和.cpp中,会导致连接错误
 5,一般将函数模板的声明和实现统一放到一个.hpp文件中
 -----------------------------
+类型转换 xx_casr<type>(expression)
+1,static_cast:一般用在去除const属性,将const转换成非const
+2,dynamic_cast
+3,reinterpret_cast
+4,const_cast
+-----------------------------
  */
 // typename == class
 
 class Point {
+    friend ostream& operator<<(ostream& cout,const Point &point);
     int m_x;
     int m_y;
 public:
-    Point(int x,int y):m_x(x),m_y(y){
+    Point(int x = 0,int y = 0):m_x(x),m_y(y){
         
     }
+};
+
+ostream& operator<<(ostream& cout,const Point &point){
+    return cout << "(" << point.m_x << "," << point.m_y << ")";
+}
+
+class Person {
+public:
+    
 };
 
 void testTemplate(void)
@@ -47,15 +63,24 @@ void testTemplate(void)
     cout << array[3] << endl;
     
     cout << array << endl;
-//    Array<Point> array2;
-//    array2.add(Point(1,2));
-//    array2.add(Point(3,4));
-////    cout << array2.get(0) << endl;
-//    cout << array2.size() << endl;
+    Array<Point> array2;
+    array2.add(Point(1,2));
+    array2.add(Point(3,4));
+//    cout << array2.get(0) << endl;
+    cout << array2 << endl;
+}
+
+void testCast(void)
+{
+    // const_cast
+    const Person* p = new Person();
+    Person* p1 = const_cast<Person*>(p);
+    cout << p1 << endl;
+    delete p;
     
 }
 
 int main(int argc, const char * argv[]) {
-    testTemplate();
+    testCast();
     return 0;
 }
