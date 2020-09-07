ff7a3f388cd8b25f05e6963a069398add4d3cb78
ALanHua <yhp-2009@qq.com>
2020-09-07 23:23:17 +0800

    其他面向对象C-2

--- c++_step_by_step_2020/day11/main.cpp
+++ c++_step_by_step_2020/day11/main.cpp
@@ -83,11 +83,68 @@ void testLanbada()
     cout << exec(30,20,[](int v1,int v2){
         return v1 / v2;
     }) << endl;
+    
+//  变量捕获
+    int a = 10;
+    int b = 20;
+    auto func = [&a,b] {
+//   auto func = [=] {// 隐式捕获
+        cout << a << endl;
+        cout << b << endl;
+    };
+    a = 50;
+    func();
+    
+    auto func1 = [&a]{
+        cout << a << endl;
+        a++;
+    };
+    
+    auto func2 = [a] ()mutable{
+        a++;// 内部变量
+        cout << "lambada: " << a << endl;
+    };
+    
+    func1();
+    cout << a << endl;
+    func2();
+    cout << a << endl;
+//------------- c++ 14------------
+    // 1，泛型lambada表达式
+    auto func3 = [](auto v1,auto v2){
+        return v1 + v2;
+    };
+    cout << func3(10,20.5) << endl;
+    // 2,对捕获的变量进行初始化
+    int c;
+    auto func4 = [c = 10](){
+        cout << c << endl;
+    };
+    func4();
+    cout << c << endl;// 未初始化
+    
+}
+
+void test01()
+{
+// ---------c++17-----------------
+    if (int a = 10;a > 10) {
+        a = 1;
+    }else if (int b = 20; a > 5 && b > 10){
+        b = 2;
+        a = 2;
+    }else if(0){
+        b = 3;
+        a = 3;
+    }else{
+        b = a;
+        a = 4;
+    }
 }
 
 int main(int argc, const char * argv[]) {
     
-    testLanbada();
+    test01();
     
     return 0;
 }
