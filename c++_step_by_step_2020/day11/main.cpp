a43c21e7b2c1b522e8f48b1e4c1095b543bdb42b
ALanHua <yhp-2009@qq.com>
2020-09-08 23:32:56 +0800

    其他面向对象C-3

--- c++_step_by_step_2020/day11/main.cpp
+++ c++_step_by_step_2020/day11/main.cpp
@@ -15,6 +15,15 @@ using namespace std;
  lambda表达式
  1,本质上就是函数
  2,完整结构 [capture list](params list) mutable exception -> return type(function type)
+ ---------------------
+ 异常
+ 1,异常是一种在程序运行过程中可能会发生的错误(比如内存不够)
+ 2,throw异常后,会在当前函数中查找匹配的catch,找不到就终止
+   当前函数代码,在上一层函数中查找,如果最终都找不到匹配的catch，整个程序就会终止
+ 3,为了增强可读性和方便团队协作,如果函数内部可能s会抛出异常
+   建议函数声明一下异常类型
+ ---------------------
+ 标准异常
  
  ---------------------
  */
@@ -142,9 +151,97 @@ void test01()
     }
 }
 
+void testException01(){
+    for (int i = 0; i < 99999; i++) {
+        try {
+            int *p = new int[99999999999];
+            /*
+             下面不会在执行
+             .......
+             ........
+             .......
+             */
+        } catch (...) {// 捕捉所有异常
+            cout << "内存不够用" << endl;
+            break;
+        }
+    }
+}
+
+void func3() throw(){// 不抛出任何异常
+    
+}
+
+int divide2(int v1,int v2)throw(const char*){// 声明抛出异常类型
+    if (v2 == 0) {
+        throw "不能除以0";
+    }
+    return v1 / v2;
+}
+
+void testException02(){
+    try {
+        int a = 10;
+        int b = 0;
+        cout << divide2(a,b) << endl;
+    } catch (const char* exception) {
+        cout << "产生异常 const char*:" << exception << endl;
+    }catch (int exception){
+        cout << "产生异常 int :" << exception << endl;
+    }
+    
+}
+
+// 自定义异常l类型
+class Exception {
+public:
+    virtual const char* what()const = 0;
+};
+
+class DivideException: public Exception {
+public:
+    const char* what()const{
+        return "不能除以0";
+    }
+};
+
+class AddException: public Exception {
+public:
+    const char* what()const{
+        return "加法有问题";
+    }
+};
+
+int divide3(int v1,int v2){// 声明抛出异常类型
+    if (v2 == 0) {
+        throw DivideException();
+    }
+    return v1 / v2;
+}
+
+void testException03(){
+//    try {
+//        int a = 10;
+//        int b = 0;
+//        cout << divide3(a,b) << endl;
+//    } catch (DivideException exception) {
+//        cout << "产生异常 DivideException()" << exception.what() << endl;
+//    }catch (AddException exception){
+//        cout << "产生异常 AddException()" << endl;
+//    }
+    
+    try {
+        int a = 10;
+        int b = 0;
+        cout << divide3(a,b) << endl;
+    } catch (const Exception& exception) {
+        cout << "产生异常 DivideException()" << exception.what() << endl;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
-    test01();
+    testException03();
     
     return 0;
 }
